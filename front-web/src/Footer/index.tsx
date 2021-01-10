e6ade5cfa94bcc48fcbb39e1640bfca338f69c6a
Erick Oliveira <erick.oliveira9607@gmail.com>
2021-01-10 19:51:22 -0300

    teste2

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -10,10 +10,10 @@ function Footer() {
                <a href="https://devsuperior.com.br/sds2-aulas4583?blog=2dwf9sqq0&video=2" target="_new">
                     <YouTube />
                </a>
-               <a href="#" target="_new">
+               <a href="https://br.linkedin.com/" target="_new">
                    <Linkedin />
                </a>
-               <a href="#">
+               <a href="https://pt.wikipedia.org/wiki/Instagram">
                 <Instagram />
                </a>
            </div>
